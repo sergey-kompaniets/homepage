02f264722d088a87191759b0b6347ffb64957c71
Sergey Kompaniets <sergey.kompaniets2@gmail.com>
2018-05-09 20:41:14 +0300

    Add creeping line and skills bar

---
+++ js/main.js
@@ -0,0 +1,17 @@
+((document) => {
+	let bars = [].slice.call(document.querySelectorAll('.bar-inner'));
+	bars.map((bar, index) => {
+		setTimeout(() => {
+			bar.style.width = bar.dataset.percent;
+		}, index * 1000);
+	})
+})(document);
+
+
+let typed = new Typed(".f-dev", {
+    strings: ["Frontend Developer ^1000", "Smart Developer ^1000", "Talented developer ^1000", "Determined Developer ^1000"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+  });
+
